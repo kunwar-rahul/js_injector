9042a1fd9215f68016bb3a74e2b88793c134d017
kunwar-rahul <kr.rahul.kunwar@gmail.com>
2018-05-04 21:53:28 +0530

    Update injector.js

--- injector.js
+++ injector.js
@@ -1,2 +1,7 @@
-setInterval(() => { window.localStorage.clear() }, 0);
-setTimeout(() => { window.location.reload() }, 18000);
+function reloader() {
+  console.log("Injected the tweek script successfully!");
+  setInterval(() => { window.localStorage.clear() }, 0);
+  setTimeout(() => { window.location.reload() }, 18000);
+}
+
+reloader();
